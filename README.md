19718df356efd98da8f1b28bfe1b6feab12a4891
TLS-Escanor <patilprajwal8804@gmail.com>
2023-05-19 17:06:22 -0800

    first commmit here

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# Movie-ticket-booking-project-in-Java-Servlet
+Project : Prajwal8674 
+
+## Using the Servlet API and MySQL database.
+
+The project is still a work in progress:
+There will be two main actors or user of the application 
+
+## Admin/Manager
+
+- [x] Admin can : Register / Login into the application.
+- [ ] Admin can handle : Add/Delete/Update any movie records.
+- [ ] Admin also can handle : Add/Delete/Update User/Customers
+
+## User/Customer
+- [x] User can : Register / Login into the application.
+- [ ] User can : View all the movies in a grid view and book tickets for those movies.
+- [ ] User can : maintain the records under the profile and can view the booking history.
+- [ ] Dummy Payment page where the user can process the payment after ticket booking and view the booked ticket after payment.
+
+---
+
+## Screenshots
---
+++ src/adminModel/adminDao.java
@@ -0,0 +1,72 @@
+package adminModel;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
+
+public class adminDao {
+	String driver = "com.mysql.jdbc.Driver";
+	String dbName = "onlinemovietickets";
+	String url    = "jdbc:mysql://localhost:3306/"+dbName;
+	String dbname ="root";
+	String dbpass = "admin01";
+	
+	Connection getConnect() throws ClassNotFoundException, SQLException{
+		
+		Class.forName(driver);
+		return  (Connection) DriverManager.getConnection(url,dbname,dbpass);
+	}
+	
+	public int addData(pojo p) throws ClassNotFoundException, SQLException{
+		String sql = "insert into admindata (username,gender,birthDate,email,password) values(?,?,?,?,?)";
+		Connection con = getConnect();
+		PreparedStatement  ps = con.prepareStatement(sql);
+		
+		ps.setString(1, p.getUname());
+		ps.setString(2, p.getUgen());
+		ps.setString(3, p.getUdob());
+		ps.setString(4, p.getUemail());
+		ps.setString(5, p.getUpass());
+
+		int a = ps.executeUpdate();
+		con.close();
+		
+		 return a;
+	}
+	
+	public boolean checkUser(String email) throws SQLException, ClassNotFoundException{
+		Class.forName(driver) ;
+		Connection con =(Connection) DriverManager.getConnection(url,dbname,dbpass);
+		PreparedStatement ps = con.prepareStatement("select * from admindata where email=?");
+		ps.setString(1, email) ;
+		ResultSet rs = ps.executeQuery() ;
+		
+		if(rs.absolute(1))
+			return true;
+			
+			con.close() ;
+			return false;						
+}
+	public boolean checkForLogin(String email , String pass ) throws ClassNotFoundException, SQLException{
+		boolean check = false;
+		Class.forName(driver);
+		Connection con =(Connection) DriverManager.getConnection(url,dbname,dbpass);
+		PreparedStatement ps = con.prepareStatement("select * from admindata where email=? and password=?");
+		
+		ps.setString(1,email);
+		ps.setString(2,pass);
+		
+		
+
+		ResultSet rs = ps.executeQuery();
+		check=rs.next();
+		
+		return check;
+		
+	}
+	
+}
